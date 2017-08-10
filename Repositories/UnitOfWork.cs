1950b03c955297f11b8ef0d4103416d85a8d49e5
Joe Shepherd <joeshepherdmba@outlook.com>
2017-08-09 20:49:10 -0700

    updated UOW

--- Repositories/UnitOfWork.cs
+++ Repositories/UnitOfWork.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+
 using Microsoft.EntityFrameworkCore;
 using ZenLeapApi.Models;
 using EF_Core_Generics.Repos;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace ZenLeapApi.Repositories
 {
@@ -71,9 +75,69 @@ namespace ZenLeapApi.Repositories
 		}
 
 
+#region Implementing IDiosposable...
+
+#region private dispose variable declaration...
+		private bool disposed = false;
+#endregion
+
+		/// <summary>
+		/// Protected Virtual Dispose method
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					Debug.WriteLine("UnitOfWork is being disposed");
+					_context.Dispose();
+				}
+			}
+			this.disposed = true;
+		}
+
+		/// <summary>
+		/// Dispose method
+		/// </summary>
 		public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-    }
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+#endregion
+
+		/// <summary>
+		/// Save method.
+		/// </summary>
+		public async Task<int> CommitChangesAsync()
+		{
+            return await _context.SaveChangesAsync();
+			//try
+			//{
+			//	_context.SaveChanges();
+			//}
+			//catch (DbEntityValidationException e)
+			//{
+
+			//	var outputLines = new List<string>();
+			//	foreach (var eve in e.EntityValidationErrors)
+			//	{
+			//		outputLines.Add(string.Format(
+			//			"{0}: Entity of type \"{1}\" in state \"{2}\" has the following validation errors:", DateTime.Now,
+			//			eve.Entry.Entity.GetType().Name, eve.Entry.State));
+			//		foreach (var ve in eve.ValidationErrors)
+			//		{
+			//			outputLines.Add(string.Format("- Property: \"{0}\", Error: \"{1}\"", ve.PropertyName, ve.ErrorMessage));
+			//		}
+			//	}
+			//	System.IO.File.AppendAllLines(@"C:\errors.txt", outputLines);
+
+			//	throw e;
+			//}
+
+		}
+
+	}
 }
